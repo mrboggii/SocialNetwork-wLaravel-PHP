2a559bc382de28e3e4417bed7e4ffcc895d1b810
mrboggii <pruteanbogdan@gmail.com>
2020-06-05 00:32:03 +0200

    dashboard taking shape

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,43 @@
-<h1>Inicio</h1>
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>Tienes algo que ocultar?</h3></header>
+            <form action="">
+                <div class="form-group">
+                    <textarea class="form-control" name="new-post" id="new-post" rows="5" placeholder="Tu publicación"></textarea>
+                </div>
+                <button type="submit" class="btn btn-primary">Publicar</button>
+        </div>
+    </section>
+    <section class="row posts">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3>Lo que ocultan los demás usuarios de Faceless</h3></header>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam ullam iure assumenda, alias voluptates ea quas nemo a debitis, maxime laboriosam voluptatum numquam doloribus, autem deleniti doloremque sunt exercitationem consequatur!</p>
+                <div class="info">
+                    Publicado por: User el 13 de Feb de 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Me encanta!</a> |
+                    <a href="#">Vaya chorrada</a> |
+                    <a href="#">Edit</a> |
+                    <a href="#">Eliminar</a>
+                </div>
+            </article>
+            <article class="post">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam ullam iure assumenda, alias voluptates ea quas nemo a debitis, maxime laboriosam voluptatum numquam doloribus, autem deleniti doloremque sunt exercitationem consequatur!</p>
+                <div class="info">
+                    Publicado por: User el 13 de Feb de 2016
+                </div>
+                <div class="interaction">
+                    <a href="#">Me encanta!</a> |
+                    <a href="#">Vaya chorrada</a> |
+                    <a href="#">Editar</a> |
+                    <a href="#">Eliminar</a>
+                </div>
+            </article>
+        </div>
+    </section>
+@endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,6 +3,7 @@
 <head>        
     <title>@yield('title')</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="{{ URL::to('src/css/main.css') }}">
 </head>
 <body>
     @include('includes.header')
